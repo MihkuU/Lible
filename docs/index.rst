b428b7bce74b1be47f89cf321c4d980a24034d0d
Mihkel Ugandi <mihkel.ugandi@hu-berlin.de>
2024-03-23 16:32:07 +0100

    Testing docs

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. Lible documentation master file, created by
+   sphinx-quickstart on Fri Mar 22 14:36:43 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Lible's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+Docs
+====
+.. doxygenstruct:: lible::ints::test
+   :members:
--- src/ints/lible/ints.hpp
+++ src/ints/lible/ints.hpp
@@ -16,6 +16,18 @@ namespace lible
 {
     namespace ints
     {
+        /**
+         * Module for calculating molecular integrals.
+         */
+        struct test
+        {
+            /**
+             * Tseburek
+             */
+
+            void testFun();
+        };
+
         typedef std::pair<std::vector<double>, std::vector<double>> shell_exps_coeffs_t;
 
         vec2d overlap(const Structure &structure);
@@ -25,45 +37,11 @@ namespace lible
         vec2d nuclearAttraction(const Structure &structure);
 
         vec2d dipoleMoment(const Structure &structure);
-        
+
         std::map<int, std::vector<shell_exps_coeffs_t>>
         returnBasisForAtom(const int atomic_nr, const std::string &basis_set);
 
         std::map<int, std::map<int, std::vector<shell_exps_coeffs_t>>>
         returnBasisForAtoms(const std::set<int> &atomic_nrs, const std::string &basis_set);
     }
-
-    // class ints
-    // {
-    // public:
-    // ints(const std::string &basis_set, const std::vector<double> &coordinates_ang,
-    //      const std::vector<std::string> &elements);
-
-    // enum Option1El
-    // {
-    //     KINETIC,
-    //     OVERLAP,
-    //     POTENTIAL
-    // };
-
-    // enum Option2El
-    // {
-    //     COULOMB,
-    //     EXCHANGE
-    // };
-
-    // template <Option1El option>
-    // std::vector<double> calcOneElInts();
-
-    // template <Option1El option>
-    // std::vector<double> calcOneElIntsGPU();
-
-    // template <Option2El option>
-    // std::vector<double> calcTwoElInts(const std::vector<double> &density); // TODO: rethink the name of this function
-
-    // struct Structure;
-    // struct StructureGPU;
-    // std::unique_ptr<Structure> structure;
-    // std::unique_ptr<StructureGPU> structure_gpu;
-    // };
 }
