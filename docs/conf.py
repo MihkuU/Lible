c1f15406174950d1da29eaa2d2f5e51c3c3b38f5
Mihkel Ugandi <mihkel.ugandi@hu-berlin.de>
2024-03-23 21:52:10 +0100

    Messing with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'MihkuU'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['breathe']
+extensions = ['breathe', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
