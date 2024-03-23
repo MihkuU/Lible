6f4f39280ea71a4908bbe860fb04cc5707eedf6a
Mihkel Ugandi <mihkel.ugandi@hu-berlin.de>
2024-03-23 21:32:28 +0100

    Messing with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ breathe_default_project = "Lible"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
 
 # -- Read the Docs Shenanigans -----------------------------------------------
