048149d46f586b690b14f1a88c329de78d70f5df
alastor <mihkel.ugandi@hotmail.com>
2025-05-09 20:56:54 +0200

    Work on (ab|c)^x kernel.

--- src/lible/ints/twoel/eri4.cpp
+++ src/lible/ints/twoel/eri4.cpp
@@ -223,135 +223,6 @@ namespace lible::ints::two
                             &eri4_shells_sph[0], dim_sph_ab);
             }
     }
-
-    void kernelERI4Deriv1(const int lab, const int lcd, const int ipair_ab, const int ipair_cd,
-                          const vector<array<int, 3>> &idxs_tuv_ab,
-                          const vector<array<int, 3>> &idxs_tuv_cd,
-                          const vector<double> &ecoeffs_ab,
-                          const vector<double> &ecoeffs_deriv1_ab,
-                          const vector<double> &ecoeffs_cd_tsp,
-                          const vector<double> &ecoeffs_deriv1_cd_tsp,
-                          const BoysGrid &boys_grid, const ShellPairData &sp_data_ab,
-                          const ShellPairData &sp_data_cd)
-    {
-        int labcd = lab + lcd;
-
-        int cdepth_a = sp_data_ab.cdepths[2 * ipair_ab];
-        int cdepth_b = sp_data_ab.cdepths[2 * ipair_ab + 1];
-        int cdepth_c = sp_data_cd.cdepths[2 * ipair_cd];
-        int cdepth_d = sp_data_cd.cdepths[2 * ipair_cd + 1];
-        int cofs_a = sp_data_ab.coffsets[2 * ipair_ab];
-        int cofs_b = sp_data_ab.coffsets[2 * ipair_ab + 1];
-        int cofs_c = sp_data_cd.coffsets[2 * ipair_cd];
-        int cofs_d = sp_data_cd.coffsets[2 * ipair_cd + 1];
-
-        const double *xyz_a = &sp_data_ab.coords[6 * ipair_ab];
-        const double *xyz_b = &sp_data_ab.coords[6 * ipair_ab + 3];    
-        const double *xyz_c = &sp_data_cd.coords[6 * ipair_cd];
-        const double *xyz_d = &sp_data_cd.coords[6 * ipair_cd + 3];
-
-        int n_sph_a = numSphericals(sp_data_ab.la);
-        int n_sph_b = numSphericals(sp_data_ab.lb);
-        int n_sph_c = numSphericals(sp_data_cd.la);
-        int n_sph_d = numSphericals(sp_data_cd.lb);
-        int n_tuv_ab = numHermites(lab);
-        int n_tuv_cd = numHermites(lcd);
-        int n_sph_ab = n_sph_a * n_sph_b;
-        int n_sph_cd = n_sph_c * n_sph_d;
-        int n_ecoeffs_ab = n_sph_ab * n_tuv_ab;
-        int n_ecoeffs_cd = n_sph_cd * n_tuv_cd;
-        int n_rints_x_ecoeffs = n_tuv_ab * n_sph_cd;
-        
-        vector<double> ecoeffs_ket(12 * n_ecoeffs_cd, 0);
-        vector<double> T_PRP_R_(cdepth_a * cdepth_b * 12 * n_rints_x_ecoeffs, 0);
-        vector<double> T_PRCD(cdepth_a * cdepth_b * 12 * n_rints_x_ecoeffs, 0);
-        for (int ic = 0, icd = 0; ic < cdepth_c; ic++)
-            for (int id = 0; id < cdepth_d; id++, icd++)
-            {                
-                int ofs_ecoeffs_cd = sp_data_cd.offsets_ecoeffs[ipair_cd] + icd * n_ecoeffs_cd;
-                for (int i = 0; i < 9; i++)
-                    std::memcpy(ecoeffs_ket.data() + i * n_ecoeffs_cd, &ecoeffs_ket[ofs_ecoeffs_cd],
-                                sizeof(double) * n_ecoeffs_cd);
-
-                int ofs_ecoeffs_deriv1_cd = sp_data_cd.offsets_ecoeffs_deriv1[ipair_cd] + 3 * icd * n_ecoeffs_cd;
-                std::memcpy(ecoeffs_ket.data() + 9 * n_ecoeffs_cd,
-                            &ecoeffs_deriv1_cd_tsp[ofs_ecoeffs_deriv1_cd],
-                            sizeof(double) * 3 * n_ecoeffs_cd);
-
-                for (int ia = 0, iab = 0; ia < cdepth_a; ia++)
-                    for (int ib = 0; ib < cdepth_b; ib++, iab++)
-                    {
-                        double a = sp_data_ab.exps[cofs_a + ia];
-                        double b = sp_data_ab.exps[cofs_b + ib];
-                        double c = sp_data_cd.exps[cofs_c + ic];
-                        double d = sp_data_cd.exps[cofs_d + id];
-
-                        double p = a + b;
-                        double q = c + d;
-                        double alpha = p * q / (p + q);
-
-                        array<double, 3> xyz_p{(a * xyz_a[0] + b * xyz_b[0]) / p,
-                                               (a * xyz_a[1] + b * xyz_b[1]) / p,
-                                               (a * xyz_a[2] + b * xyz_b[2]) / p};
-
-                        array<double, 3> xyz_q{(c * xyz_c[0] + d * xyz_d[0]) / q,
-                                               (c * xyz_c[1] + d * xyz_d[1]) / q,
-                                               (c * xyz_c[2] + d * xyz_d[2]) / q};
-
-                        array<double, 3> xyz_pq{xyz_p[0] - xyz_q[0], xyz_p[1] - xyz_q[1],
-                                                xyz_p[2] - xyz_q[2]};
-
-                        double xx{xyz_pq[0]}, xy{xyz_pq[1]}, xz{xyz_pq[2]};
-                        double x = alpha * (xx * xx + xy * xy + xz * xz);                        
-
-                        // Gota increment l by 1 for the R(t + t' + 1, u + u', v + v')...
-                        vector<double> fnx = calcBoysF(labcd + 1, x, boys_grid); 
-
-                        double fac = (2.0 * std::pow(M_PI, 2.5) / (p * q * std::sqrt(p + q)));
-
-                        vector<double> rints = calcRInts_ERI4_Deriv1(labcd, fac, alpha, xyz_pq.data(),
-                                                                     fnx.data(), idxs_tuv_ab,
-                                                                     idxs_tuv_cd);
-
-                        // P
-                        // R 
-                        // P'
-                        // R'
-
-                        // // Trafo |P'R') -> |CD)
-                        // for (int i = 0; i < n_sph_a; i++)
-                        //     for (int j = 0; j < n_sph_b; j++)
-                        //         for (size_t tuv = 0; tuv < idxs_tuv_cd.size(); tuv++)
-                        //         {
-                        //             int ofs_P_ = ;
-                        //             int ofs_R_;
-                        //         }
-                    }
-            }
-
-        vector<double> ecoeffs_bra(12 * n_ecoeffs_ab, 0);
-        vector<double> ints_PRCD;
-        vector<double> ints_ABCD;        
-        for (int ia = 0, iab = 0; ia < cdepth_a; ia++)
-            for (int ib = 0; ib < cdepth_b; ib++, iab++)
-            {
-                int ofs_ecoeffs_ab = sp_data_ab.offsets_ecoeffs[ipair_ab] + iab * n_ecoeffs_ab;
-                for (int i = 0; i < 3; i++)
-                    std::memcpy(ecoeffs_bra.data() + i * n_ecoeffs_ab, &ecoeffs_ab[ofs_ecoeffs_ab],
-                                sizeof(double) * n_ecoeffs_ab);
-
-                for (int i = 6; i < 12; i++)
-                    std::memcpy(ecoeffs_bra.data() + i * n_ecoeffs_ab, &ecoeffs_ab[ofs_ecoeffs_ab],
-                                sizeof(double) * n_ecoeffs_ab);
-
-                int ofs_ecoeffs_deriv1_ab = sp_data_ab.offsets_ecoeffs_deriv1[ipair_ab] + 3 * iab * n_ecoeffs_ab;
-                std::memcpy(ecoeffs_bra.data() + 3 * n_ecoeffs_ab,
-                            &ecoeffs_deriv1_ab[ofs_ecoeffs_deriv1_ab],
-                            sizeof(double) * 3 * n_ecoeffs_ab);
-
-                // Trafo (PR| -> (AB|
-            }
-    }
 }
 
 lible::vec4d LIT::calcERI4(const Structure &structure)
@@ -790,7 +661,7 @@ void LIT::kernelERI4Deriv1(const int la, const int lb, const int lc, const int l
                     vector<double> rints = calcRInts_ERI4_Deriv1(labcd, fac, alpha, xyz_pq.data(),
                                                                  fns.data(), idxs_tuv_ab,
                                                                  idxs_tuv_cd);
-
+                            
                     // P
                     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n_hermite_ab, n_sph_cd,
                                 n_hermite_cd, 1.0, &rints[0 * n_hermite_abcd], n_hermite_cd,
@@ -822,6 +693,8 @@ void LIT::kernelERI4Deriv1(const int la, const int lb, const int lc, const int l
                                 n_hermite_cd, 1.0, &rints[3 * n_hermite_abcd], n_hermite_cd,
                                 &ecoeffs_cd_tsp[icd * n_ecoeffs_cd], n_sph_cd, 1.0,
                                 &R_x_E[ofs_R_x_E + 5 * n_R_x_E], n_sph_cd);
+
+                    std::fill(R_x_E_P_R_.begin(), R_x_E_P_R_.end(), 0);
 
                     // P'
                     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n_hermite_ab, n_sph_cd,
