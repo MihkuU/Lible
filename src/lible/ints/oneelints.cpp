6eb8aa73655f3df0ea98be8226ac14d91014e4d7
Mihkel Ugandi <mihkel.ugandi@hu-berlin.de>
2026-01-19 22:01:55 +0100

    Fixed sign error in momentumKernel

--- src/lible/ints/oneelints.cpp
+++ src/lible/ints/oneelints.cpp
@@ -1491,9 +1491,12 @@ std::array<lible::vec2d, 3> lints::momentum(const Structure &structure)
                     for (size_t nu = 0; nu < ints_ipair[0].dim<1>(); nu++)
                         for (int icart = 0; icart < 3; icart++)
                         {
-                            ints[icart](ofs_a + mu, ofs_b + nu) = ints_ipair[icart](mu, nu);
+                            size_t i = ofs_a + mu;
+                            size_t j = ofs_b + nu;
+                            ints[icart](i, j) = ints_ipair[icart](mu, nu);
                             // derivative integrals are antisymmetric
-                            ints[icart](ofs_b + nu, ofs_a + mu) = -1 * ints_ipair[icart](mu, nu);
+                            if (i != j)
+                                ints[icart](i, j) = -1 * ints_ipair[icart](mu, nu);
                         }
             }
         }
